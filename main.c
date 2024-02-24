ea41a247bd75a872e9c1bd216515dd9c1e2902e6
linvegas <saulo.costam@gmail.com>
2024-02-24 14:31:49 -0300

    Rendering cols and rows of enemies

--- main.c
+++ main.c
@@ -3,8 +3,11 @@
 #include <stdio.h>
 #include <string.h>
 
-#define N_BULLETS 10
-#define N_ENEMIES 10
+#define ENEMIES_ROW 4
+#define ENEMIES_COL 10
+#define ENEMIES_GAP 20
+#define ENEMIES_COUNT ENEMIES_ROW*ENEMIES_COL
+#define BULLETS_COUNT 10
 
 typedef struct {
     Rectangle rect;
@@ -18,6 +21,13 @@ typedef struct {
     float     speed;
     int       activated;
 } Enemy;
+
+typedef struct {
+    Rectangle rect;
+    Color     color;
+    float     speed;
+    float     thickness;
+} EnemyArea;
 
 typedef struct {
     Rectangle rect;
@@ -42,21 +52,36 @@ int main(void)
         .color = GREEN,
     };
 
-    Enemy enemies[N_ENEMIES];
+    const int SCREEN_PADDING_X = 50;
+    const int SCREEN_PADDING_Y = 25;
 
-    for (int i = 0; i < N_ENEMIES; i++) {
-        enemies[i].rect.width = 30;
-        enemies[i].rect.height = 30;
-        enemies[i].rect.x = (enemies[i].rect.width + 20) * (i + 1);
-        enemies[i].rect.y = SCREEN_H * 0.12;
-        enemies[i].speed = 2;
-        enemies[i].activated = 1;
-        enemies[i].color = LIGHTGRAY;
+    Enemy enemies[ENEMIES_ROW][ENEMIES_COL];
+
+    for (int i = 0; i < ENEMIES_ROW; i++) {
+        for (int j = 0; j < ENEMIES_COL; j++) {
+            enemies[i][j].rect.width = 30;
+            enemies[i][j].rect.height = 30;
+            enemies[i][j].rect.x = (enemies[i][j].rect.width + ENEMIES_GAP) * j + SCREEN_PADDING_X;
+            enemies[i][j].rect.y = (enemies[i][j].rect.height + ENEMIES_GAP) * i + SCREEN_PADDING_Y;
+            enemies[i][j].speed = 1;
+            enemies[i][j].activated = 1;
+            enemies[i][j].color = LIGHTGRAY;
+        }
     }
 
-    Bullet bullets[N_BULLETS];
+    EnemyArea enemy_area = {
+        .rect.x = enemies[0][0].rect.x,
+        .rect.y = enemies[0][0].rect.y,
+        .rect.width = enemies[0][0].rect.width * ENEMIES_COL + (ENEMIES_GAP * ENEMIES_COL) - ENEMIES_GAP,
+        .rect.height = enemies[0][0].rect.height * ENEMIES_ROW + (ENEMIES_GAP * ENEMIES_ROW) - ENEMIES_GAP,
+        .thickness = 2,
+        .speed = 1,
+        .color = RED,
+    };
 
-    for (int i = 0; i < N_BULLETS; i++) {
+    Bullet bullets[BULLETS_COUNT];
+
+    for (int i = 0; i < BULLETS_COUNT; i++) {
         bullets[i].rect.x = ship.rect.x;
         bullets[i].rect.y = SCREEN_H - SCREEN_H * 0.1;
         bullets[i].rect.width = 3;
@@ -69,15 +94,9 @@ int main(void)
     SetTargetFPS(60);
 
     int score = 0;
-    int screen_padding = 50;
-    int enemies_alive = N_ENEMIES;
+    int enemies_alive = ENEMIES_COUNT;
 
     while(!WindowShouldClose()) {
-
-        // for (int i = 0; i < N_BULLETS; i++) {
-        //     printf("%d ", bullets[i].activated);
-        // }
-        // printf("\n");
 
         if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_L)) ship.rect.x += 2.0f * ship.speed;
         if (IsKeyDown(KEY_LEFT) ||  IsKeyDown(KEY_H)) ship.rect.x -= 2.0f * ship.speed;
@@ -86,7 +105,7 @@ int main(void)
         if (ship.rect.x <= 0) ship.rect.x = 0;
 
         if (IsKeyPressed(KEY_SPACE)) {
-            for (int i = 0; i < N_BULLETS; i++) {
+            for (int i = 0; i < BULLETS_COUNT; i++) {
                 if (!bullets[i].activated) {
                     bullets[i].rect.x = ship.rect.x + ((ship.rect.width / 2) - (bullets[i].rect.width / 2));
                     bullets[i].rect.y = SCREEN_H - SCREEN_H * 0.1;
@@ -96,7 +115,7 @@ int main(void)
             }
         }
 
-        for (int i = 0; i < N_BULLETS; i++) {
+        for (int i = 0; i < BULLETS_COUNT; i++) {
             if (bullets[i].activated) {
 
                 bullets[i].rect.y -= bullets[i].speed;
@@ -105,33 +124,43 @@ int main(void)
                     bullets[i].activated = 0;
                 }
 
-                for (int j = 0; j < N_ENEMIES; j++) {
-                    if (enemies[j].activated) {
-                        if (CheckCollisionRecs(bullets[i].rect, enemies[j].rect)) {
-                            score++;
-                            bullets[i].activated = 0;
-                            enemies[j].activated = 0;
-                            enemies_alive--;
+                for (int j = 0; j < ENEMIES_ROW; j++) {
+                    for (int k = 0; k < ENEMIES_COL; k++) {
+                        if (enemies[j][k].activated) {
+                            if (CheckCollisionRecs(bullets[i].rect, enemies[j][k].rect)) {
+                                score++;
+                                bullets[i].activated = 0;
+                                enemies[j][k].activated = 0;
+                                enemies_alive--;
+                            }
                         }
                     }
                 }
             }
         }
 
-        for (int i = 0; i < N_ENEMIES; i++) {
-            if (enemies[i].activated) {
-                enemies[i].rect.x += enemies[i].speed;
-                if ((enemies[i].rect.x + enemies[i].rect.width >= SCREEN_W - screen_padding) || (enemies[i].rect.x <= screen_padding)) enemies[i].speed *= -1.0f;
+        enemy_area.rect.x += enemy_area.speed;
+        if ((enemy_area.rect.x + enemy_area.rect.width >= SCREEN_W - SCREEN_PADDING_X) || (enemy_area.rect.x <= SCREEN_PADDING_X)) enemy_area.speed *= -1.0f;
+
+        for (int i = 0; i < ENEMIES_ROW; i++) {
+            for (int j = 0; j < ENEMIES_COL; j++) {
+                if (enemies[i][j].activated) {
+                    enemies[i][j].rect.x += enemies[i][j].speed;
+                    if ((enemy_area.rect.x + enemy_area.rect.width >= SCREEN_W - SCREEN_PADDING_X) || (enemy_area.rect.x <= SCREEN_PADDING_X)) enemies[i][j].speed *= -1.0f;
+                }
             }
         }
 
         if (enemies_alive <= 0) {
-            for (int i = 0; i < N_ENEMIES; i++) {
-                enemies[i].rect.x = (enemies[i].rect.width + 20) * (i + 1);
-                enemies[i].speed = 2;
-                enemies[i].activated = 1;
+            for (int i = 0; i < ENEMIES_ROW; i++) {
+                for (int j = 0; j < ENEMIES_COL; j++) {
+                    enemies[i][j].rect.x = (enemies[i][j].rect.width + ENEMIES_GAP) * j + enemy_area.rect.x;
+                    enemies[i][j].rect.y = (enemies[i][j].rect.height + ENEMIES_GAP) * i + enemy_area.rect.y;
+                    enemies[i][j].speed = enemy_area.speed;
+                    enemies[i][j].activated = 1;
+                }
             }
-            enemies_alive = N_ENEMIES;
+            enemies_alive = ENEMIES_COUNT;
         }
 
         BeginDrawing();
@@ -140,11 +169,16 @@ int main(void)
 
         DrawRectangleRec(ship.rect, ship.color);
 
-        for (int i = 0; i < N_BULLETS; i++)
+        for (int i = 0; i < BULLETS_COUNT; i++)
             if (bullets[i].activated) DrawRectangleRec(bullets[i].rect, bullets[i].color);
 
-        for (int i = 0; i < N_ENEMIES; i++)
-            if (enemies[i].activated) DrawRectangleRec(enemies[i].rect, enemies[i].color);
+        for (int i = 0; i < ENEMIES_ROW; i++) {
+            for (int j = 0; j < ENEMIES_COL; j++) {
+                    if (enemies[i][j].activated) DrawRectangleRec(enemies[i][j].rect, enemies[i][j].color);
+            }
+        }
+
+        DrawRectangleLinesEx(enemy_area.rect, enemy_area.thickness, enemy_area.color);
 
         DrawText(TextFormat("Score: %i", score), 10, SCREEN_H - 45 + 20, 20, WHITE);
 
